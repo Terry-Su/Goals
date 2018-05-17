f3a8f679e4accd32ef1bf3e804660e3bcfb42a51
TerrySu <2684759855@qq.com>
2018-05-17 22:11:01 +0800

    Refactor: [Time Tag] Convert cn to en when locale is en

--- src/js/util/lang/getTimingInfo.js
+++ src/js/util/lang/getTimingInfo.js
@@ -1,4 +1,5 @@
 import moment from 'moment'
+import { isNumber, isString, isNaN } from 'lodash'
 
 import { getLanguage } from '../../store/localStore'
 
@@ -21,7 +22,7 @@ const getTimingInfo = (startDate, endDate) => {
 
 
   if (isTimeline || isRecycle) {
-    switch(currentLanguage) {
+    switch (currentLanguage) {
       case 'zh':
         return '定时'
       case 'en':
@@ -32,34 +33,51 @@ const getTimingInfo = (startDate, endDate) => {
 
   if (isBefore) {
     const s = moment().to(startDate, true)
-    switch(currentLanguage) {
+    switch (currentLanguage) {
       case 'zh':
         return s.replace(/ /g, "") + '后开始'
       case 'en':
-        return 'Start after ' + s
+        return 'Start after ' + convertCnTimeStringToEn(s)
     }
   }
 
   if (isDoing) {
     const s = moment().to(endDate, true)
-    switch(currentLanguage) {
+    switch (currentLanguage) {
       case 'zh':
         return s.replace(/ /g, "") + '后结束'
       case 'en':
-        return 'End after ' + s
+        return 'End after ' + convertCnTimeStringToEn(s)
     }
   }
 
   if (isAfter) {
     const s = moment().to(endDate, true)
-    switch(currentLanguage) {
+    switch (currentLanguage) {
       case 'zh':
         return '超时' + s.replace(/ /g, "")
       case 'en':
-        return 'Timeout: ' + s
+        return 'Timeout: ' + convertCnTimeStringToEn(s)
     }
   }
 }
 
 
 export default getTimingInfo
+
+
+function convertCnTimeStringToEn(s) {
+  const potentialNumberString = s.replace('秒', '').replace('分钟', '').replace('小时', '').replace('天', '').replace('月', '').replace('年', '')
+  const potentialNumber = parseInt(potentialNumberString)
+  const isNaNNumber = isNaN(potentialNumber)
+  if (isNaNNumber) {
+    return s
+  }
+  if (!isNaNNumber) {
+    if (potentialNumber === 1) {
+      return potentialNumber + s.replace(potentialNumberString, '   ').replace('秒', 'second').replace('分钟', 'minute').replace('小时', 'hour').replace('天', 'day').replace('月', 'month').replace('年', 'year')
+    } else {
+      return potentialNumber + s.replace(potentialNumberString, '  ').replace('秒', 'seconds').replace('分钟', 'minutes').replace('小时', 'hours').replace('天', 'days').replace('月', 'months').replace('年', 'years')
+    }
+  }
+}
